6d7c5876eb5e0f4e75d35d090232386b323c25a9
tomerIQ713 <tomermazurov@gmail.com>
2026-06-18 22:22:49 +0300

    fix Express 5 catch-all route syntax

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ app.use('/api', assignmentRoutes)
 app.use('/api', messageRoutes)
 
 app.use(express.static(path.join(__dirname, '..', 'dist')))
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'))
 })
 
